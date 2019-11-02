f3f3e5b754feaa290508934573497080d3ea4872
rohan <rsureshkumar@empyra.com>
2019-11-02 15:39:32 +0530

    Updates

---
+++ precache-manifest.00888ba331a573fd1e9d04af58376142.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f846b310eb3559a43f16253108c083",
+    "url": "/color-changing-box/index.html"
+  },
+  {
+    "revision": "a3f87fa2e6700d99cb7a",
+    "url": "/color-changing-box/static/css/main.28f7168e.chunk.css"
+  },
+  {
+    "revision": "484428c7ea1635925dbb",
+    "url": "/color-changing-box/static/js/2.3017d388.chunk.js"
+  },
+  {
+    "revision": "a3f87fa2e6700d99cb7a",
+    "url": "/color-changing-box/static/js/main.dca0c767.chunk.js"
+  },
+  {
+    "revision": "7f830ffd019aa88ceb5a",
+    "url": "/color-changing-box/static/js/runtime-main.81d83a40.js"
+  }
+]);
