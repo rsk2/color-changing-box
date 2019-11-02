14205a0dfc714cd9e4910dfa0e5ca4d3e3ea85f6
rohan <rsureshkumar@empyra.com>
2019-11-02 14:41:06 +0530

    Changing logic to display Color box. Adding new link for available colors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 import ColorBox from './components/ColorBox.js';
 import './index.css';
 
 
-
 class ColorChanger extends Component {
 state = {
-    color: "",
+    color: "red",
 }
 
 changeColorBox= () => {
@@ -19,17 +19,20 @@ changeColorBox= () => {
 
     render() {
         return (
-        <div>
-            {this.state.color == "" ? <ColorBox inputColor="red"/> : <ColorBox inputColor= {this.state.color}/>}
+        <div className="box">
+            <ColorBox inputColor= {this.state.color}/>
             <input type="text"  id="newColor"/> 
              <button onClick={this.changeColorBox}>Change</button>
              <br/>
              You have entered the color: {this.state.color}
+             <br/>
+             Get list of available colors <a href="https://www.w3schools.com/colors/colors_names.asp" target="_blank">here</a>
+          
         </div>
           
         );
     }
 }
 
-ReactDOM.render(<ColorChanger/>, document.getElementById('root'));
+ReactDOM.render(<ColorChanger />, document.getElementById('root'));
 
